664f5550a462e055304ede2d74754bc0beec70ec
lawlietlol <iamlawliet@bk.ru>
2018-03-05 23:53:07 +0300

    Upload project.

---
+++ Training/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Training
+{
+    public partial class Form1 : Form
+    {
+        int count = 1;
+        string inpRus;
+        string inpEng;
+        public static Dictionary<string, string> dictionary;
+        bool check = false;
+        public static string name = Environment.UserName;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Enter eng word";
+            dictionary = new Dictionary<string, string>(1);
+        }
+        //Заполнение словаря
+        private void Save_To_Dict(string eng, string rus)
+        {
+            dictionary.Add(eng, rus);
+        }
+        //Тупо вывод в листбоксы
+        private void Save_To_ListBoxes()
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox1.Items.AddRange(dictionary.Keys.ToArray<string>());
+            listBox2.Items.AddRange(dictionary.Values.ToArray<string>());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check = false;
+            label1.Text = "Enter eng word";
+            Save_To_Dict(inpEng, inpRus);
+            Save_To_ListBoxes();
+            textBox1.Focus();
+            //SelectNextControl(textBox1, true, true, false, false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new TrainingForm().Show();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+                if (!check)
+                {
+                    inpEng = textBox1.Text;
+                    textBox1.Text = String.Empty;
+                    label1.Text = "Enter rus definition";
+                    check = true;
+                }
+                else
+                {
+                    inpRus = textBox1.Text;
+                    textBox1.Text = String.Empty;
+                    label1.Text = "Press add btn";
+                    button1.Focus();
+                }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //if ((e.KeyCode == Keys.Enter) || (e.KeyCode == Keys.Return))
+            //{
+            //    this.SelectNextControl((Control)sender, true, true, true, true);
+            //}
+        }
+    }
+}
---
+++ Training/TrainingForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Training
+{
+    public partial class TrainingForm : Form
+    {
+        private List<string> list;
+        private int count;
+        private string[] keys = Form1.dictionary.Keys.ToArray<string>();
+
+
+        public TrainingForm()
+        {
+            InitializeComponent();
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+
+            list = new List<string> { "Hello there, voyager!", $"or better call you {Form1.name}",
+                "There is no way out...", "...until you have finish...", "... this awesome challenge!", "First question:"};
+            count = list.Count;
+        }
+
+        //private void Introdution(object count)
+        //{
+        //    for (int i = 0; i < (int)count; i++)
+        //    {
+        //        label1.Invoke(label1.Text = list[i];
+        //        Thread.Sleep(3000);
+        //    }
+        //}
+
+        private void Introdution()
+        {
+            for (int i = 0; i < count; i++)
+            { 
+                Invoke(new Action(() => { label1.Text = list[i]; }));
+                Thread.Sleep(2000);
+            }
+        }
+        // Допилить логику цикла
+        private void Challenge()
+        {
+            int numQuesions = keys.Length;
+
+            for (int i = 0; i < numQuesions; i++)
+            {
+                if (textBox1.Text == keys[i])
+                    label1.Text = "Well done";
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter && textBox1.Text == keys[0])
+            {
+                MessageBox.Show("Well done");
+            }
+            else
+                MessageBox.Show("Nope");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Introdution);
+            //th.Start();
+            label1.Text = $"What is {keys[0].ToString()}?";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
